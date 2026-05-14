5dbd0a3c4443f95e216e6a12214fd6df01320382
AndiWatkins <andiwatkins22@gmail.com>
2026-05-13 20:37:58 -0700

    completed

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ var height = prompt("Enter the height", 150);
 var width = prompt("Enter the width", 230);
     
 // Write your code below
-
-
+let area = height * width;
 
 
 // Print out result in the console log statement below 
-console.log();
+console.log(area);
